7dca7ee1553d8bfa6a66ee3a69ee318481bf003e
Termincc <112787855+Termincc@users.noreply.github.com>
2024-01-01 21:32:47 +1000

    working on calc

--- auroramarketing/calc.js
+++ auroramarketing/calc.js
@@ -1,4 +1,10 @@
 const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+const input3 = document.getElementById("input3")
+const dropdown = document.getElementById("dropdown")
+
+
+
 
 
 input1.addEventListener("input", () => {
@@ -8,4 +14,23 @@ input1.addEventListener("input", () => {
         document.getElementById("box1").classList.add('expanded')
     }
 })
+
+input2.addEventListener("input", () => {
+    if (input2.value === "") {
+        document.getElementById("box2").classList.remove('expanded')
+    } else {
+        document.getElementById("box2").classList.add('expanded')
+    }
+})
+
+input3.addEventListener("input", () => {
+    if (input3.value === "") {
+        document.getElementById("box3").classList.remove('expanded')
+    } else {
+        document.getElementById("box3").classList.add('expanded')
+    }
+})
+
+
+//let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
 
