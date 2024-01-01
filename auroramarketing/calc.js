164e6486709f86704dd8972d7993c6b011588be2
Termincc <112787855+Termincc@users.noreply.github.com>
2024-01-01 21:25:51 +1000

    new design

--- auroramarketing/calc.js
+++ auroramarketing/calc.js
@@ -1,49 +1,11 @@
-const opt1 = document.getElementById("opt1")
-const opt2 = document.getElementById("opt2")
-const opt2SelectType = document.getElementById("opt2SelectType")
-const optMonthlySalary = document.getElementById("opt2opt1")
-const optHourlyRate = document.getElementById("opt2opt2")
-const optCasualRate = document.getElementById("opt2opt3")
-const opt3 = document.getElementById("opt3")
-var opt2SalaryType
+const input1 = document.getElementById("input1")
 
 
-optMonthlySalary.addEventListener("click", () => {
-    opt2SelectType.innerHTML = "Monthly Salary"
-    opt2SalaryType = "monthlySalary"
-})
-
-optHourlyRate.addEventListener("click", () => {
-    opt2SelectType.innerHTML = "Hourly Rate"
-    opt2SalaryType = "hourlyRate"
-})
-
-optCasualRate.addEventListener("click", () => {
-    opt2SelectType.innerHTML = "Casual Rate"
-    opt2SalaryType = "casualRate"
-    test()
-})
-
-
-
-
-
-opt1.addEventListener("input", () => {
-    if (opt1.value === "") {
-        reset("1")
-    }
-    else {
-        opt1Calc()
+input1.addEventListener("input", () => {
+    if (input1.value === "") {
+        document.getElementById("box1").classList.remove('expanded')
+    } else {
+        document.getElementById("box1").classList.add('expanded')
     }
 })
 
-
-function reset(option) {
-    document.getElementById("box"+option).style.height = "200px"
-}
-
-function opt1Calc() {
-    let startVal = parseFloat(opt1.value)
-    document.getElementById("box1").style.height = "600px"
-    
-}
