77a0c5992c8d42df207354f4921b712f67dc169e
Termincc <112787855+Termincc@users.noreply.github.com>
2024-01-02 22:07:59 +1000

    finishing calc

--- auroramarketing/calc.js
+++ auroramarketing/calc.js
@@ -9,28 +9,76 @@ const dropdown = document.getElementById("dropdown")
 
 input1.addEventListener("input", () => {
     if (input1.value === "") {
-        document.getElementById("box1").classList.remove('expanded')
+        document.getElementById("results1").innerHTML = ""
     } else {
-        document.getElementById("box1").classList.add('expanded')
+        let annualSalary = parseFloat(input1.value)
+        let monthlySalary = (annualSalary / 12).toFixed(2)
+        let monthlySuper = ((monthlySalary / 100) * 11).toFixed(2) //This 11 is the super than changes every year!
+        let hourlyRate = ((annualSalary / 52) / 38).toFixed(2)
+        let casualRate = (hourlyRate * 1.25).toFixed(2)
+        let casualLoading = ((hourlyRate / 100) * 25).toFixed(2)
+        let labourCostPerm = (hourlyRate * 1.25).toFixed(2)
+        let labourCostCasual = (casualRate * 1.2).toFixed(2)
+
+
+        document.getElementById("results1").innerHTML = `<p>Monthly Salary:        $`+ monthlySalary +`</p>
+<p>Monthly Super:         $`+ monthlySuper +`</p>
+<p>Hourly Rate:           $`+ hourlyRate +`</p>
+<p>Casual Rate:           $`+ casualRate +`</p>
+<p>Casual Loading:        $`+ casualLoading +`</p>
+<p>Labour Cost (Perm):    $`+ labourCostPerm +`</p>
+<p>Labour Cost (Casual):  $`+ labourCostCasual +`</p>`
     }
 })
 
 input2.addEventListener("input", () => {
     if (input2.value === "") {
-        document.getElementById("box2").classList.remove('expanded')
+        document.getElementById("results2").innerHTML = ""
     } else {
-        document.getElementById("box2").classList.add('expanded')
+        let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
+        let value = parseFloat(input2.value)
+        if (type == "Monthly Salary") {
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 12)).toFixed(2)
+        } else if (type == "Hourly Rate") {
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 38) * 52).toFixed(2)
+        } else { //Casual Rate
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + (((value / 1.25) * 38) * 52).toFixed(2)
+        }
     }
 })
 
 input3.addEventListener("input", () => {
     if (input3.value === "") {
-        document.getElementById("box3").classList.remove('expanded')
+        document.getElementById("results3").innerHTML = ""
     } else {
-        document.getElementById("box3").classList.add('expanded')
+        let annualSalary = parseFloat(input3.value)
+        let monthlySalary = (annualSalary / 12).toFixed(2)
+        let twoWeeksAnnual = (annualSalary * (50 / 52)).toFixed(2)
+        let twoWeeksMonthly = (monthlySalary * (50 / 52)).toFixed(2)
+        let fourWeeksAnnual = (annualSalary * (48 / 52)).toFixed(2)
+        let fourWeeksMonthly = (monthlySalary * ( 48 / 52)).toFixed(2)
+
+        document.getElementById("results3").innerHTML = `<p>2 Weeks Time for Living (Annual Rate): $`+ twoWeeksAnnual +`</p>
+<p>2 Weeks Time for Living (Monthly Rate): $`+ twoWeeksMonthly +`</p>
+<p>4 Weeks Time for Living (Annual Rate): $`+ fourWeeksAnnual +`</p>
+<p>4 Weeks Time for Living (Monthly Rate): $`+ fourWeeksMonthly +`</p>`
     }
 })
 
 
-//let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
+dropdown.addEventListener("input", () => {
+    if (input2.value === "") {
+        document.getElementById("results2").innerHTML = ""
+    } else {
+        let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
+        let value = parseFloat(input2.value)
+        if (type == "Monthly Salary") {
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 12)).toFixed(2)
+        } else if (type == "Hourly Rate") {
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 38) * 52).toFixed(2)
+        } else { //Casual Rate
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + (((value / 1.25) * 38) * 52).toFixed(2)
+        }
+    }
+})
 
