21c3df60de47155d8eebc44d4b2383f2300281d2
Termincc <112787855+Termincc@users.noreply.github.com>
2024-01-03 21:53:22 +1000

    fix occasional rounding errors

--- auroramarketing/calc.js
+++ auroramarketing/calc.js
@@ -12,13 +12,13 @@ input1.addEventListener("input", () => {
         document.getElementById("results1").innerHTML = ""
     } else {
         let annualSalary = parseFloat(input1.value)
-        let monthlySalary = (annualSalary / 12).toFixed(2)
-        let monthlySuper = ((monthlySalary / 100) * 11).toFixed(2) //This 11 is the super than changes every year!
-        let hourlyRate = ((annualSalary / 52) / 38).toFixed(2)
-        let casualRate = (hourlyRate * 1.25).toFixed(2)
-        let casualLoading = ((hourlyRate / 100) * 25).toFixed(2)
-        let labourCostPerm = (hourlyRate * 1.25).toFixed(2)
-        let labourCostCasual = (casualRate * 1.2).toFixed(2)
+        let monthlySalary = Math.round(((annualSalary / 12) + Number.EPSILON)*100)/100
+        let monthlySuper = Math.round((((monthlySalary / 100) * 11) + Number.EPSILON)*100)/100 //This 11 is the super than changes every year!
+        let hourlyRate = Math.round((((annualSalary / 52) / 38) + Number.EPSILON)*100)/100
+        let casualRate = Math.round(((hourlyRate * 1.25) + Number.EPSILON)*100)/100
+        let casualLoading = Math.round((((hourlyRate / 100) * 25) + Number.EPSILON)*100)/100
+        let labourCostPerm = Math.round(((hourlyRate * 1.25) + Number.EPSILON)*100)/100
+        let labourCostCasual = Math.round(((casualRate * 1.2) + Number.EPSILON)*100)/100
 
 
         document.getElementById("results1").innerHTML = `<p>Monthly Salary:        $`+ monthlySalary +`</p>
@@ -38,11 +38,11 @@ input2.addEventListener("input", () => {
         let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
         let value = parseFloat(input2.value)
         if (type == "Monthly Salary") {
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 12)).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round((((value * 12)) + Number.EPSILON)*100)/100
         } else if (type == "Hourly Rate") {
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 38) * 52).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round((((value * 38) * 52) + Number.EPSILON)*100)/100
         } else { //Casual Rate
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + (((value / 1.25) * 38) * 52).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round(((((value / 1.25) * 38) * 52) + Number.EPSILON)*100)/100
         }
     }
 })
@@ -52,11 +52,11 @@ input3.addEventListener("input", () => {
         document.getElementById("results3").innerHTML = ""
     } else {
         let annualSalary = parseFloat(input3.value)
-        let monthlySalary = (annualSalary / 12).toFixed(2)
-        let twoWeeksAnnual = (annualSalary * (50 / 52)).toFixed(2)
-        let twoWeeksMonthly = (monthlySalary * (50 / 52)).toFixed(2)
-        let fourWeeksAnnual = (annualSalary * (48 / 52)).toFixed(2)
-        let fourWeeksMonthly = (monthlySalary * ( 48 / 52)).toFixed(2)
+        let monthlySalary = Math.round(((annualSalary / 12) + Number.EPSILON)*100)/100
+        let twoWeeksAnnual = Math.round(((annualSalary * (50 / 52)) + Number.EPSILON)*100)/100
+        let twoWeeksMonthly = Math.round(((monthlySalary * (50 / 52)) + Number.EPSILON)*100)/100
+        let fourWeeksAnnual = Math.round(((annualSalary * (48 / 52)) + Number.EPSILON)*100)/100
+        let fourWeeksMonthly = Math.round(((monthlySalary * ( 48 / 52)) + Number.EPSILON)*100)/100
 
         document.getElementById("results3").innerHTML = `<p>2 Weeks Time for Living (Annual Rate): $`+ twoWeeksAnnual +`</p>
 <p>2 Weeks Time for Living (Monthly Rate): $`+ twoWeeksMonthly +`</p>
@@ -73,11 +73,11 @@ dropdown.addEventListener("input", () => {
         let type = document.getElementById("dropdown").options[document.getElementById("dropdown").selectedIndex].text
         let value = parseFloat(input2.value)
         if (type == "Monthly Salary") {
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 12)).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round((((value * 12)) + Number.EPSILON)*100)/100
         } else if (type == "Hourly Rate") {
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + ((value * 38) * 52).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round((((value * 38) * 52) + Number.EPSILON)*100)/100
         } else { //Casual Rate
-            document.getElementById("results2").innerHTML = "Annual Salary: $" + (((value / 1.25) * 38) * 52).toFixed(2)
+            document.getElementById("results2").innerHTML = "Annual Salary: $" + Math.round(((((value / 1.25) * 38) * 52) + Number.EPSILON)*100)/100
         }
     }
 })
